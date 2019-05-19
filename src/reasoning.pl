5d5d9ff6a12e0897c61d809d6ebfc3fca08dd977
Aleksandra <al.byczynska@gmail.com>
2019-05-19 22:58:48 +0200

    relevance

--- src/reasoning.pl
+++ src/reasoning.pl
@@ -1,7 +1,7 @@
 :- consult('database.pl').
 :- consult('rules.pl').
 
-:- dynamic(progress/2).
+:- dynamic(progress/3).
 :- dynamic(result/2).
 
 start_reasoning :-
@@ -17,27 +17,30 @@ reason_person(Id) :-
 
 calculate_score([], Score, Score).
 calculate_score([{Feature, Value} | _], _, 0) :-
-    get_answer(Feature, Answer),
-    score(Feature, Answer, Value, NewScore),
+    get_answer(Relevance, Feature, Answer),
+    score(Relevance, Feature, Answer, Value, NewScore),
     NewScore =< 0, !.
 calculate_score([{Feature, Value} | Tail], OldScore, Score) :-
-    get_answer(Feature, Answer),
-    score(Feature, Answer, Value, NewScore),
+    get_answer(Relevance, Feature, Answer),
+    score(Relevance, Feature, Answer, Value, NewScore),
     TempScore is min(OldScore, NewScore),
     calculate_score(Tail, TempScore, Score).
 
-get_answer(Feature, Answer) :-
-    progress(Feature, Answer).
-get_answer(Feature, Answer) :-
-    \+ progress(Feature, Answer),
-    ask(Feature, Answer).
+get_answer(Relevance, Feature, Answer) :-
+    progress(Relevance, Feature, Answer).
+get_answer(Relevance, Feature, Answer) :-
+    \+ progress(Relevance, Feature, Answer),
+    ask(Relevance, Feature, Answer).
 
-ask(Feature, Answer) :-
+ask(Relevance, Feature, Answer) :-
+    relevance_question(Feature, RelevanceQuestion),
+    write(RelevanceQuestion), write(' '), read(Relevance), nl,
+    % TODO zrobic zeby sie pytalo o pytanie tylko jesli Relevance jest wieksze niz 0.
     question(Feature, Question),
     write(Question), write(' '), read(Answer), nl,
-    assertz(progress(Feature, Answer)).
+    assertz(progress(Relevance, Feature, Answer)).
 
 clear_reasoning :-
-    retractall(progress(_, _)),
+    retractall(progress(_, _, _)),
     retractall(result(_, _)), !.
 clear_reasoning.
--- src/rules.pl
+++ src/rules.pl
@@ -2,7 +2,7 @@
 % possible features
 % -----------------
 
-feature(age,    'Age').
+feature(age, 'Age').
 feature(gender, 'Gender').
 feature(height, 'Height').
 feature((lon, lat), 'Location').
@@ -12,6 +12,7 @@ feature(education, 'Education').
 feature(occupation, 'Occupation').
 feature(religion, 'Religion').
 feature(orientation, 'Sexual orientation').
+feature((family, work, hobbies, travelling, wellness, spirituality, partying, development)), 'Life priorities')
 
 % --------------------
 % features' properties
@@ -28,10 +29,13 @@ feature_type(occupation, atom, [teacher, scientist, driver, doctor, programmer, 
     comedian, writer, politician, model, architect, engineer, mechanic, designer, unemployed]).
 feature_type(religion, atom, [christian, jewish, hindu, muslim, buddhist, atheist, other]).
 feature_type(orientation, atom, [heterosexual, homosexual, bisexual, asexual, pansexual]).
+feature_type((family, work, hobbies, travelling, wellness, spirituality, partying, development), 
+(number, number, number, number, number, number, number, number), 
+([1, 5], [1, 5], [1, 5], [1, 5], [1, 5], [1, 5], [1, 5], [1, 5])).
 
-% ------------------------
-% questions about features
-% ------------------------
+% --------------------------------------------
+% questions about features and their relevance
+% --------------------------------------------
 
 question(age, 'What should be his/her age?').
 question(gender, 'What should be his/her gender?').
@@ -43,6 +47,22 @@ question(education, 'What level of education should he/she have?').
 question(occupation, 'What occupation should he/she have?').
 question(religion, 'What should his/her religious affiliation be?').
 question(orientation, 'What should be his/her sexual orientation').
+question((family, work, hobbies, travelling, wellness, spirituality, partying, development), 
+'How important (from 1 (not important) to 5 (very important) are the following life priorities to you: ' +
+'family, work, hobbies, travelling, wellness, spirituality, partying, self-development').
+
+relevance_question(age, 'How much do you care about his/her age (from 0 to 1)?').
+relevance_question(gender, 'How much do you care about his/her gender (from 0 to 1)?').
+relevance_question(height, 'How much do you care about his/her height (from 0 to 1)?').
+relevance_question((lon, lat), 'How much do you care about his/her location (from 0 to 1)?').
+relevance_question(attractiveness, 'How much do you care about his/her attractiveness level(from 0 to 1)?').
+relevance_question(income, 'How much do you care about his/her income (from 0 to 1)?').
+relevance_question(education, 'How much do you care about his/her level of education (from 0 to 1)?').
+relevance_question(occupation, 'How much do you care about his/her occupation? (from 0 to 1)?').
+relevance_question(religion, 'How much do you care about his/her religious affiliation (from 0 to 1)?').
+relevance_question(orientation, 'How much do you care about his/her sexual orientation (from 0 to 1)?').
+relevance_question((family, work, hobbies, travelling, wellness, spirituality, partying, development), 
+'How much do you care about matching his/her life priorities (from 0 to 1)?').
 
 % ---------------
 % score functions
@@ -51,48 +71,79 @@ question(orientation, 'What should be his/her sexual orientation').
 deg_to_rad(Deg, Rad) :-
     Rad is Deg * pi / 180.
 
-score(age, Answer, Value, Score) :-
-    Score is max(0, 1 - abs(Answer - Value) / 20).
+score(Relevance, age, Answer, Value, Score) :-
+    Relevance =:= 0
+    -> Score is 1
+    ;  Score is max(0, 1 - (abs(Answer - Value) / 20) * Relevance).
 
-score(gender, Answer, Value, 0) :- 
-    Answer \= Value, !.
-score(gender, _, _, 1).
+score(Relevance, gender, Answer, Value, Score) :- 
+    Relevance =:= 0 ; Answer \= Value
+    -> Score is 1
+    ;  Score is 1 - Relevance.
 
-score(height, Answer, Value, Score) :-
-    Score is max(0, 1 - abs(Answer - Value) / 40).
+score(Relevance, height, Answer, Value, Score) :-
+    Relevance =:= 0
+    -> Score is 1
+    ;  Score is max(0, 1 - (abs(Answer - Value) / 40) * Relevance).
 
-score((lon, lat), (LonAns, LatAns), (LonVal, LatVal), Score) :-
-    (LonAns =:= LonVal ), (LatAns =:= LatVal)
-    ->  Score is 1
-    ;   deg_to_rad(LatAns, LatAnsRad), 
-        deg_to_rad(LonAns, LonAnsRad),
-        deg_to_rad(LatVal, LatValRad),
-        deg_to_rad(LonVal, LonValRad),
-        Score is
-        max(0, 1 - abs(6366 * acos(
-            (cos(LatAnsRad) * cos(LonAnsRad) * cos(LatValRad) * cos(LonValRad)) + 
-            (cos(LatAnsRad) * sin(LonAnsRad) * cos(LatValRad) * sin(LonValRad)) + 
-            (sin(LatAnsRad) * sin(LatValRad))) / 500)
-            ).
+score(Relevance, (lon, lat), (LonAns, LatAns), (LonVal, LatVal), Score) :-
+    Relevance =:= 0
+    -> Score is 1
+    ;  (LonAns =:= LonVal), (LatAns =:= LatVal)
+        ->  Score is 1
+        ;   deg_to_rad(LatAns, LatAnsRad), 
+            deg_to_rad(LonAns, LonAnsRad),
+            deg_to_rad(LatVal, LatValRad),
+            deg_to_rad(LonVal, LonValRad),
+            Score is
+            max(0, 1 - (abs(6366 * acos(
+                (cos(LatAnsRad) * cos(LonAnsRad) * cos(LatValRad) * cos(LonValRad)) + 
+                (cos(LatAnsRad) * sin(LonAnsRad) * cos(LatValRad) * sin(LonValRad)) + 
+                (sin(LatAnsRad) * sin(LatValRad))) / 500)
+                ) * Relevance).
 
-score(attractiveness, Answer, Value, Score) :-
-    Score is max(0, 1 - abs(Answer - Value) / 100).
 
-score(income, Answer, Value, Score) :-
-    Score is max(0, 1 - abs(Answer - Value) / Answer).
+score(Relevance, (family, work, hobbies, travelling, wellness, spirituality, partying, development), 
+(familyAns, workAns, hobbiesAns, travellingAns, wellnessAns, spiritualityAns, partyingAns, developmentAns),
+(familyVal, workVal, hobbiesVal, travellingVal, wellnessVal, spiritualityVal, partyingVal, developmentVal), Score) :-
+    Relevance =:= 0
+    -> Score is 1
+    ;  Score is max(0, 1 - ((abs(familyAns - familyVal) + 
+                                abs(worksAns - worksVal) +
+                                abs(familyAns - familyVal) + 
+                                abs(hobbiesAns - hobbiesVal) +
+                                abs(travellingAns - travellingVal) +
+                                abs(wellnessAns - wellnesVal) +
+                                abs(spiritualityAns - spiritualityVal) +
+                                abs(partyingAns - partyingVal) +
+                                abs(developmentAns - developmentVal)) / 16) * Relevance).
+
+score(Relevance, attractiveness, Answer, Value, Score) :-
+    Relevance =:= 0
+    -> Score is 1
+    ;  Score is max(0, 1 - (abs(Answer - Value) / 100) * Relevance).
+
+score(Relevance, income, Answer, Value, Score) :-
+    Relevance =:= 0
+    -> Score is 1
+    ;  Score is max(0, 1 - (abs(Answer - Value) / Answer) * Relevance).
     
-score(education, Answer, Value, 0) :-
-    Answer \= Value, !.
-score(education, _, _, 1).
+score(Relevance, education, Answer, Value, Score) :-
+    Relevance =:= 0 ; Answer \= Value
+    -> Score is 1
+    ;  Score is 1 - Relevance.
 
-score(religion, Answer, Value, 0) :-
-    Answer \= Value, !.
-score(religion, _, _, 1).
+score(Relevance, religion, Answer, Value, Score) :-
+    Relevance =:= 0 ; Answer \= Value
+    -> Score is 1
+    ;  Score is 1 - Relevance.
 
-score(occupation, Answer, Value, 0) :-
-    Answer \= Value, !.
-score(occupation, _, _, 1).
+score(Relevance, occupation, Answer, Value, Score) :-
+    Relevance =:= 0 ; Answer \= Value
+    -> Score is 1
+    ;  Score is 1 - Relevance.
 
-score(orientation, Answer, Value, 0) :-
-    Answer \= Value, !.
-score(orientation, _, _, 1).
+score(Relevance, orientation, Answer, Value, Score) :-
+    Relevance =:= 0 ; Answer \= Value
+    -> Score is 1
+    ;  Score is 1 - Relevance.
