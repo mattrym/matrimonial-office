4480b86be365f89315a5f4da0903a6b398a22961
Aleksandra <al.byczynska@gmail.com>
2019-05-21 16:37:09 +0200

    lists

--- src/reasoning.pl
+++ src/reasoning.pl
@@ -15,15 +15,21 @@ reason_person(Id) :-
     calculate_score(FVs, 1, Score), Score > 0,
     assertz(result(Id, Score)).
 
+get_score(OldScore, NewScore, Score) :-
+    Score is (OldScore - ((1- NewScore) / 11)).
+    % Score is NewScore.
+
 calculate_score([], Score, Score).
-calculate_score([{Feature, Value} | _], _, 0) :-
+calculate_score([{Feature, Value} | _], OldScore, 0) :-
     get_answer(Feature, Answer),
     score(Feature, Answer, Value, NewScore),
-    NewScore =< 0, !.
+    get_score(OldScore, NewScore, TempScore),
+    TempScore =< 0.5, !.
 calculate_score([{Feature, Value} | Tail], OldScore, Score) :-
     get_answer(Feature, Answer),
     score(Feature, Answer, Value, NewScore),
-    TempScore is min(OldScore, NewScore),
+    % TempScore is min(OldScore, NewScore),
+    get_score(OldScore, NewScore, TempScore),
     calculate_score(Tail, TempScore, Score).
 
 get_answer(Feature, Answer) :-
--- src/rules.pl
+++ src/rules.pl
@@ -18,6 +18,9 @@ feature((family, work, hobbies, travelling, wellness, spirituality, partying, de
 % features' properties
 % --------------------
 
+
+
+
 feature_type(age, number, [18, 99]).
 feature_type(gender, atom, [male, female]).
 feature_type(height, number, [130, 250]).
@@ -25,8 +28,8 @@ feature_type((lon, lat), (number, number), ([0, 180], [0, 90])).
 feature_type(attractiveness, number, [0, 100]).
 feature_type(income, number, [0, 1000000000]).
 feature_type(education, atom, [elementary, secondary, academic]).
-feature_type(occupation, atom, [teacher, scientist, driver, doctor, programmer, actor, singer, cook, waiter, lawyer, sportsman, 
-    comedian, writer, politician, model, architect, engineer, mechanic, designer, unemployed]).
+feature_type(occupation, [atom], [[teacher, scientist, driver, doctor, programmer, actor, singer, cook, waiter, lawyer, sportsman, 
+    comedian, writer, politician, model, architect, engineer, mechanic, designer, unemployed]]).
 feature_type(religion, atom, [christian, jewish, hindu, muslim, buddhist, atheist, other]).
 feature_type(orientation, atom, [heterosexual, homosexual, bisexual, asexual, pansexual]).
 feature_type((family, work, hobbies, travelling, wellness, spirituality, partying, development), 
@@ -38,15 +41,15 @@ feature_type((family, work, hobbies, travelling, wellness, spirituality, partyin
 % ------------------------
 
 question(age, 'What should be his/her age?').
-question(gender, 'What should be his/her gender?').
+question(gender, 'What should be his/her gender? [male, female] ').
 question(height, 'What should be his/her height?').
-question((lon, lat), 'What location (lon, lat) should he/she be living in').
-question(attractiveness, 'What should be his/her attractiveness level?').
-question(income, 'What should be his/her income (yearly)?').
-question(education, 'What level of education should he/she have?').
-question(occupation, 'What occupation should he/she have?').
-question(religion, 'What should his/her religious affiliation be?').
-question(orientation, 'What should be his/her sexual orientation').
+question((lon, lat), 'What location (lon, lat) should he/she be living in? ([0, 180], [0, 90])').
+question(attractiveness, 'What should be his/her attractiveness level? [0, 100]').
+question(income, 'What should be his/her income (yearly)? [0, 1000000000]').
+question(education, 'What level of education should he/she have? [elementary, secondary, academic]').
+question(occupation, 'What occupation should he/she have? [teacher, scientist, driver, doctor, programmer, actor, singer, cook, waiter, lawyer, sportsman, comedian, writer, politician, model, architect, engineer, mechanic, designer, unemployed]').
+question(religion, 'What should his/her religious affiliation be? [christian, jewish, hindu, muslim, buddhist, atheist, other]').
+question(orientation, 'What should be his/her sexual orientation? [heterosexual, homosexual, bisexual, asexual, pansexual]').
 question((family, work, hobbies, travelling, wellness, spirituality, partying, development), 
 'How important (from 1 (not important) to 5 (very important) are the following life priorities to you: family, work, hobbies, travelling, wellness, spirituality, partying, self-development').
 
@@ -60,9 +63,13 @@ deg_to_rad(Deg, Rad) :-
 score(age, Answer, Value, Score) :-
     Score is max(0, 1 - abs(Answer - Value) / 20).
 
-score(gender, Answer, Value, 0) :- 
-    Answer \= Value, !.
-score(gender, _, _, 1).
+score(gender, Answer, Value, 1) :-
+    Answer = Value.
+score(gender, [], _, 0).
+score(gender, [H|T], Value, Score) :-
+    H = Value
+    ->  Score is 1
+    ;   score(gender, T, Value, Score).
 
 score(height, Answer, Value, Score) :-
     Score is max(0, 1 - abs(Answer - Value) / 40).
@@ -87,21 +94,33 @@ score(attractiveness, Answer, Value, Score) :-
 score(income, Answer, Value, Score) :-
     Score is max(0, 1 - abs(Answer - Value) / Answer).
     
-score(education, Answer, Value, 0) :-
-    Answer \= Value, !.
-score(education, _, _, 1).
+score(education, Answer, Value, 1) :-
+    Answer = Value.
+score(education, [], _, 0).
+score(education, [H|T], Value, Score) :-
+    H = Value
+    ->  Score is 1
+    ;   score(education, T, Value, Score).
 
 score(religion, Answer, Value, 0) :-
     Answer \= Value, !.
 score(religion, _, _, 1).
 
-score(occupation, Answer, Value, 0) :-
-    Answer \= Value, !.
-score(occupation, _, _, 1).
+score(occupation, Answer, Value, 1) :-
+    Answer = Value.
+score(occupation, [], _, 0).
+score(occupation, [H|T], Value, Score) :-
+    H = Value
+    ->  Score is 1
+    ;   score(occupation, T, Value, Score).
 
-score(orientation, Answer, Value, 0) :-
-    Answer \= Value, !.
-score(orientation, _, _, 1).
+score(orientation, Answer, Value, 1) :-
+    Answer = Value.
+score(orientation, [], _, 0).
+score(orientation, [H|T], Value, Score) :-
+    H = Value
+    ->  Score is 1
+    ;   score(orientation, T, Value, Score).
 
 score((family, work, hobbies, travelling, wellness, spirituality, partying, development), (FamilyAns, WorkAns, HobbiesAns, TravellingAns, WellnessAns, SpiritualityAns, PartyingAns, DevelopmentAns), (FamilyVal, WorkVal, HobbiesVal, TravellingVal, WellnessVal, SpiritualityVal, PartyingVal, DevelopmentVal), Score) :-
     Score is max(0, 1 - ((abs(FamilyAns - FamilyVal) + 
